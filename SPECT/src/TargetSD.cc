31f8423e4aefa465321864914066a66f0ce8d9e1
yuema137 <3124558229@qq.com>
2024-07-17 10:14:00 -0500

    add data export

--- SPECT/src/TargetSD.cc
+++ SPECT/src/TargetSD.cc
@@ -1,5 +1,6 @@
 #include "TargetSD.hh"
 #include "G4RunManager.hh"
+#include "G4AnalysisManager.hh"
 
 namespace SPECT
 {
@@ -15,24 +16,64 @@ void TargetSD::Initialize(G4HCofThisEvent*)
 
 }
 
-G4bool TargetSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
+G4bool TargetSD::ProcessHits(G4Step* step, G4TouchableHistory*)
 {
     // Get the track associated with this step
-    G4Track* track = aStep->GetTrack();
+    G4Track* track = step->GetTrack();
 
-    // Get the pre-step point (beginning of the step)
-    G4StepPoint* preStepPoint = aStep->GetPreStepPoint();
+    // Get the pre-step and post-step points
+    G4StepPoint* preStepPoint = step->GetPreStepPoint();
+    G4StepPoint* postStepPoint = step->GetPostStepPoint();
 
-    // Get the post-step point (end of the step)
-    G4StepPoint* postStepPoint = aStep->GetPostStepPoint();
+    // Get positions at the beginning and end of the step
+    G4ThreeVector preStepPosition = preStepPoint->GetPosition();
+    G4ThreeVector postStepPosition = postStepPoint->GetPosition();
 
-    // Get the position of the particle at the beginning of the step
-    G4ThreeVector posPhoton = preStepPoint->GetPosition();
+    // Get the process name
+    G4String processName = postStepPoint->GetProcessDefinedStep()->GetProcessName();
+    
+    // Get the energy deposit
+    G4double energyDeposit = step->GetTotalEnergyDeposit();
 
-    // Print the position of the photon
-    G4cout << "Photon position: " << posPhoton << G4endl;
+    // Get the analysis manager
+    G4AnalysisManager* analysisManager = G4AnalysisManager::Instance();
+    
+    // Get the current event ID
+    G4int eventID = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
 
-    // Return true to indicate that the hit was processed successfully
+    // Get particle information
+    G4String particleName = track->GetParticleDefinition()->GetParticleName();
+    G4int particlePDG = track->GetDefinition()->GetPDGEncoding();
+    G4String parentParticleName = track->GetParentID() == 0 ? "None" : 
+        track->GetDynamicParticle()->GetParticleDefinition()->GetParticleName();
+
+    // Get track information
+    G4int trackID = track->GetTrackID();
+    G4int parentTrackID = track->GetParentID();
+
+    // Get kinetic energies
+    G4double preStepKineticEnergy = preStepPoint->GetKineticEnergy();
+    G4double postStepKineticEnergy = postStepPoint->GetKineticEnergy();
+
+    // Fill the ntuple
+    analysisManager->FillNtupleIColumn(0, eventID);
+    analysisManager->FillNtupleDColumn(1, preStepPosition.x());
+    analysisManager->FillNtupleDColumn(2, preStepPosition.y());
+    analysisManager->FillNtupleDColumn(3, preStepPosition.z());
+    analysisManager->FillNtupleDColumn(4, postStepPosition.x());
+    analysisManager->FillNtupleDColumn(5, postStepPosition.y());
+    analysisManager->FillNtupleDColumn(6, postStepPosition.z());
+    analysisManager->FillNtupleDColumn(7, energyDeposit);
+    analysisManager->FillNtupleSColumn(8, processName);
+    analysisManager->FillNtupleSColumn(9, particleName);
+    analysisManager->FillNtupleSColumn(10, parentParticleName);
+    analysisManager->FillNtupleIColumn(11, trackID);
+    analysisManager->FillNtupleIColumn(12, parentTrackID);
+    analysisManager->FillNtupleIColumn(13, particlePDG);
+    analysisManager->FillNtupleDColumn(14, preStepKineticEnergy);
+    analysisManager->FillNtupleDColumn(15, postStepKineticEnergy);
+    analysisManager->AddNtupleRow();
+
     return true;
 }
 
